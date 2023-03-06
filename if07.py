a65d616afea20e85ad5fd87e5b889c3d83e780f3
stranik87 <tulkin.boynazarov@gmail.com>
2023-03-06 15:24:57 +0500

    if07

--- if07.py
+++ if07.py
@@ -12,4 +12,20 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if (a%2 == 1) > 0:
+        print("положительное нечетное число")
+
+    if (a%2 == 0)> 1:
+        print("положительное четное число")
+
+    if (a%2 == 1) < 0:
+        print( "отрицательное нечетное число")
+    if (a%2 == 0) < 0:
+        print("отрицательное четное число")    
+
+    if a == 0:
+        print( "число равно нулю")    
+    return a
+
+
+print(main(0))
